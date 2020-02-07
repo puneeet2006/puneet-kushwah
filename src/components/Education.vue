20fd3abbb0b9846ff89dd30a25208fe05871cd02
puneeet2006 <puneet.2006@outlook.com>
2020-02-07 18:13:48 +0530

    edit projects

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -10,18 +10,18 @@
             <q-timeline-entry heading class="text-grey">
               Bachelor of Computer Science Engineer (B.E)
             </q-timeline-entry>
-            <q-timeline-entry title="Bachelor of Engineering | Computer Science / Indore, India" icon="done_all" subtitle="2009-2013">
+            <q-timeline-entry title="Bachelor of Engineering | Computer Science / Indore, India" icon="fas fa-graduation-cap" subtitle="2009-2013">
               <div>
                 Bachelor of Engineering from Sri Aurobindo Institute of Technology, Indore affiliated to RGPV, Bhopal
                 with specialization in Computer Science (2009-2013) (Aggregate: 70%)
               </div>
             </q-timeline-entry>
-            <q-timeline-entry title="Higher Secondary School Certificate (12th) | Mathematics / Dewas, India" icon="done_all" subtitle="2008-2009">
+            <q-timeline-entry title="Higher Secondary School Certificate (12th) | Mathematics / Dewas, India" icon="fas fa-graduation-cap" subtitle="2008-2009">
               <div>
                 Higher Secondary School Certificate from B.C.M Junior College Dewas, M.P 2008-2009 (Percentage 83%)
               </div>
             </q-timeline-entry>
-            <q-timeline-entry title="Secondary School Certificate (10th) | Dewas, India" icon="done_all" subtitle="2006-2007">
+            <q-timeline-entry title="Secondary School Certificate (10th) | Dewas, India" icon="fas fa-graduation-cap" subtitle="2006-2007">
               <div>
                 Secondary School Certificate from B.C.M Junior College Dewas, M.P 2006-2007 (Percentage 89%)
               </div>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -11,7 +11,7 @@
               <q-card class="project-card">
                 <img class="responsive project-img" :src="item.front.imgscr">
                 <q-card-section>
-                  <q-btn fab color="pink" @click="onClick(item.id)" icon="fas fa-ellipsis-v" class="absolute project-fab-btn" />
+                  <q-btn fab color="pink" @click="flipCard(item.id)" icon="fas fa-ellipsis-v" class="absolute project-fab-btn" />
                   <div class="row no-wrap items-center">
                     <div class="col text-h6 ">
                       <div class="text-primary">{{item.front.name}}</div>
@@ -22,20 +22,21 @@
               </q-card>
             </div>
             <div slot="back">
-              <q-card class="project-card">
-                <q-card-section>
-                  <div class="full-width">
-                    <div class="text-h5 text-primary ">Tech Used</div>
-                    <div class="text-subtitle2 row justify-end">by John Doe</div>
+              <q-card class="project-card row">
+                <q-card-section class="row full-width justify-between">
+                  <div class="text-h5 text-primary ">Accomplishments</div>
+                  <div class="cursor-pointer" @click="flipCard(item.id)">
+                    <q-icon size="2em" color="pink" name="fas fa-arrow-circle-left" />
                   </div>
                 </q-card-section>
-                <q-card-section>
-                  asdsadas
-                </q-card-section>
+                <div class="full-width">
+                  <ul v-for="data in item.back.tech">
+                    <li class="text-grey">{{data}}</li>
+                  </ul>
+                </div>
                 <q-separator dark />
-                <q-card-actions>
-                  <q-btn flat>Action 1</q-btn>
-                  <q-btn flat>Action 2</q-btn>
+                <q-card-actions class="full-width self-end justify-end">
+                  <q-btn fab color="primary" size="sm" icon="fas fa-external-link-alt" @click="gotToExternal(item.back.to)" />
                 </q-card-actions>
               </q-card>
             </div>
@@ -61,43 +62,70 @@ export default
         flipped:false,
             front: {
               imgscr:'/statics/jtconsulting.png',
-              name:'JT Consulting Bangkok',
+              name:'JT Consulting Thailand',
               desc:'Leading recruitment firm in Bangkok'
             },
-            back: {}
+            back: {
+              tech:[
+              'Build with Quasar Framework',
+              'Front end Vue JS, SASS',
+              'Backend Firebase',
+              'Multi-lingual'
+
+              ],
+              to:"https://jtconsultings.com/"
+            }
         },
       {
         id:"proj2",
         flipped:false,
             front: {
-              imgscr:'/statics/dentalwarehouse.png',
-              name:'Dental Warehouse Australia',
-              desc:'Dental equipement ecommerce site operated in Australia'
+              imgscr:'/statics/vsr.png',
+              name:'VSR Rolling Mills India',
+              desc:'Rolling mills product info'
 
             },
-            back: {}
+            back: { tech:[
+              'Build with Quasar Framework',
+              'Front end Vue JS, SASS',
+              'Backend Firebase',
+              'Admin panel for product management'
+
+              ],
+              to:"https://vsrrollingmills.com/"
+            }
       },
             {
         id:"proj3",
         flipped:false,
             front: {
-              imgscr:'/statics/dentalwarehouse.png',
-              name:'Dental Warehouse Australia',
-              desc:'Dental equipement ecommerce site operated in Australia'
+              imgscr:'/statics/carohit.png',
+              name:'CA Rohit Jaiswal',
+              desc:'Application manages client Tax documents'
 
             },
-            back: {}
+            back: { tech:[
+              'Developed using Laravel PHP Framework',
+              'Front end using Bootstrap, SASS',
+              'Backend MySQL',
+              'Admin panel for client management'
+              ],
+              to:'https://carohitjaiswal.com/'}
       },
       {
         id:"proj4",
         flipped:false,
             front: {
-              imgscr:'/statics/dentalwarehouse.png',
-              name:'Dental Warehouse Australia',
-              desc:'Dental equipement ecommerce site operated in Australia'
-
+              imgscr:'/statics/smiledental.png',
+              name:'Smile Dental Clinic',
+              desc:'Dentist profile Application'
             },
-            back: {}
+            back: { tech:[
+              'Developed using HTML, CSS, jQuery',
+              'Front end using mdBootstrap',
+              
+              ],
+              to:''}
       },
       {
         id:"proj5",
@@ -108,7 +136,16 @@ export default
               desc:'Dental equipement ecommerce site operated in Australia'
 
             },
-            back: {}
+            back: {
+              tech:[
+              'Build with Quasar Framework',
+              'Front end Vue JS, SASS',
+              'Backend Firebase',
+              'Multi-lingual'
+
+              ],
+              to:"https://jtconsultings.com/"
+            }
       }
     ]
       }
@@ -119,10 +156,14 @@ export default
   },
   methods:
   {
-    onClick(id)
+    flipCard(id)
     {
       const indexOfProduct = this.projectdata.findIndex(product=> product.id === id);
       this.projectdata[indexOfProduct].flipped = !this.projectdata[indexOfProduct].flipped;
+    },
+    gotToExternal(url)
+    {
+      window.open(url, '_blank');
     }
   }
 
